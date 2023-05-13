5809056a655f188b06c233c79d384b605b25b601
NeiDer1123 <neybm1123@gmail.com>
2023-05-13 18:16:16 -0500

    Implementacion de Redux

--- client/src/components/CardsContainer/CardsContainer.jsx
+++ client/src/components/CardsContainer/CardsContainer.jsx
@@ -1,74 +1,11 @@
+import { useSelector } from "react-redux";
 import Card from "../Card/Card";
 import style from "./CardsContainer.module.css";
 
 export default function CardsContainer() {
-  const pokemons = [
-    {
-      id: 1,
-      name: "bulbasaur",
-      image:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
-      life: 45,
-      attack: 49,
-      defense: 49,
-      speed: 45,
-      height: 7,
-      weight: 69,
-      types: ["grass", "poison"],
-    },
-    {
-      id: 2,
-      name: "ivysaur",
-      image:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg",
-      life: 60,
-      attack: 62,
-      defense: 63,
-      speed: 60,
-      height: 10,
-      weight: 130,
-      types: ["grass", "poison"],
-    },
-    {
-      id: 3,
-      name: "venusaur",
-      image:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg",
-      life: 80,
-      attack: 82,
-      defense: 83,
-      speed: 80,
-      height: 20,
-      weight: 1000,
-      types: ["grass", "poison"],
-    },
-    {
-      id: 4,
-      name: "charmander",
-      image:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg",
-      life: 39,
-      attack: 52,
-      defense: 43,
-      speed: 65,
-      height: 6,
-      weight: 85,
-      types: ["fire"],
-    },
-    {
-      id: 5,
-      name: "charmeleon",
-      image:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg",
-      life: 58,
-      attack: 64,
-      defense: 58,
-      speed: 80,
-      height: 11,
-      weight: 190,
-      types: ["fire"],
-    },
-  ];
+
+  const pokemons = useSelector(state=>state.pokemons)
+
   return (
     <div className={style.container}>
       {pokemons.map((poke) => {
