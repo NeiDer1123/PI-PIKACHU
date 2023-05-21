b671eb967d4d6214198d28d63b620cd7290c1e38
NeiDer1123 <neybm1123@gmail.com>
2023-05-20 19:14:23 -0500

    Estilos Landing Pager

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,7 +1,16 @@
-export default function Landing(){
-    return(
-        <div>
-            <h1>Esta el la vista del LANDING</h1>
-        </div>
-    )
+import { NavLink } from "react-router-dom";
+import style from "./Landing.module.css";
+import logo from "../../assets/logo.png";
+
+export default function Landing() {
+  return (
+    <div className={style.container}>
+      <img src={logo} alt="logo" className={style.logo} />
+      <div>
+        <NavLink to="/home" className={style.navlink}>
+          HOME
+        </NavLink>
+      </div>
+    </div>
+  );
 }
