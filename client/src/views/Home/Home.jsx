8521f0a14ca9fe9b8d0bc7036538514ef0415bf2
NeiDer1123 <neybm1123@gmail.com>
2023-05-30 19:33:46 -0500

    Cambio dispatch Home

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,13 +1,14 @@
 import { CardsContainer} from "../../components";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { getAll} from "../../redux/actions";
+import { getAll, getPokemons} from "../../redux/actions";
 
 export default function Home() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAll());
+    dispatch(getPokemons())
   }, [dispatch]);
 
   return (
